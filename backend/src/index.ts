fb714ab7763c8132723eb50df39a8b6d17d588ef
Prabhat kumar gupta <guptajim3636@gmail.com>
2024-01-25 23:45:51 +0530

    backend setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,16 @@
-import express from "express";
+import express , {Request,Response}from "express";
 import cors from "cors";
 import "dotenv/config";
+
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
+
+app.get("/api/test",async(req:Request,res:Response)=>{
+    res.json({message:"hello from express"})
+})
+
+app.listen(4500,()=>{
+    console.log(`Server is running at 4500`)
+})
