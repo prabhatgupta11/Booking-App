c0ce8526fd145e973dbbc0546424f6dd4b810270
Prabhat kumar gupta <guptajim3636@gmail.com>
2024-01-26 19:37:24 +0530

    register and login done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,33 @@
 import express , {Request,Response}from "express";
 import cors from "cors";
 import "dotenv/config";
+import  mongoose from "mongoose";
+import userRouter from "./routes/users";
+import loginRouter from "./routes/auth";
+import cookieParser from "cookie-parser";
+const run = async () => {
+    await mongoose.connect(process.env.MONGO_URL as string,)
+    console.log("Connected to myDB");
+  }
+  
+  run()
+  .catch((err) => console.error(err))
+
 
 const app=express();
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
-app.use(cors());
+app.use(cors(
+  {
+    origin:process.env.FRONTEND_URL,
+    credentials:true,
+  }
+));
 
-app.get("/api/test",async(req:Request,res:Response)=>{
-    res.json({message:"hello from express"})
-})
+
+app.use("/api/user",userRouter)
+
 
 app.listen(4500,()=>{
     console.log(`Server is running at 4500`)
---
+++ backend/src/routes/users.ts
@@ -0,0 +1,113 @@
+import express, { Request, Response } from "express";
+
+import { check, validationResult } from "express-validator";
+import User from "../models/user";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
+import verifyToken from "../middleware/auth"
+
+const userRouter = express.Router();
+
+
+// Regiseter Api
+userRouter.post("/register",[
+    check("firstName","First Name is required").notEmpty(),
+    check("lastName","Last Name is required").notEmpty(),
+    check("email","Email Name is required").isEmail(),
+    check("password","Password  with 6 or more charactor  is required").isLength({min:6}),
+], async (req: Request, res: Response) => {
+ 
+    const errors=validationResult(req);
+   
+    if(!errors.isEmpty()){
+        return res.status(400).json({message:errors.array()})
+    }
+  try {
+    let user = await User.findOne({
+      email: req.body.email,
+    });
+ 
+    if (user) {
+      return res.status(400).json({ message: "Userr Already Exist" });
+    }
+    user = new User(req.body);
+
+    await user.save();
+
+    const token = jwt.sign(
+      { userId: user.id },
+      process.env.JWT_SECRET_KEY as string,
+      {
+        expiresIn: "1d",
+      }
+    );
+    res.cookie("auth_token",token,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV === "production",
+        maxAge:86400000,
+    })
+    return res.status(200).send({message:"User Register Ok"});
+  } catch (error) {
+    console.log("32",error);
+    res.status(500).send({ message: "User Not register !Error" });
+  }
+});
+
+
+userRouter.post(
+  "/login",
+  [
+    check("email", "Email is required").isEmail(),
+    check(
+      "password",
+      "Password with 6 or more charactor is required "
+    ).isLength({
+      min: 6,
+    }),
+  ],
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ message: errors.array() });
+    }
+
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(400).json({ message: "Invalid Credential" });
+      }
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch) {
+        return res.status(400).json({ message: "Invalid Password" });
+      }
+
+      const token = jwt.sign(
+        { userId: user.id },
+        process.env.JWT_SECRET_KEY as string,
+        {
+          expiresIn: "1d",
+        }
+      );
+
+      res.cookie("auth_token",token,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV === "production",
+        maxAge:86400000,
+    })
+    return res.status(200).json({userId:user._id});
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ message: "Error while login" });
+    }
+  }
+);
+
+userRouter.get("/validate-token",verifyToken,(req:Request,res:Response)=>{
+  res.status(200).send({userId:req.userId})
+});
+export default userRouter;
+
+
