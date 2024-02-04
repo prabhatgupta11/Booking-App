f39c1826d8dd89ad8f668e03dd97ab274ee0d84b
Prabhat kumar gupta <guptajim3636@gmail.com>
2024-02-04 19:47:11 +0530

    check4

---
+++ backend/index.js
@@ -0,0 +1,40 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+require("dotenv/config");
+const mongoose_1 = __importDefault(require("mongoose"));
+const users_1 = __importDefault(require("./dist/routes/users"));
+const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const path_1 = __importDefault(require("path"));
+const run = () => __awaiter(void 0, void 0, void 0, function* () {
+    yield mongoose_1.default.connect(process.env.MONGODB_CONNECTION_STING);
+    console.log("Connected to myDB");
+});
+run()
+    .catch((err) => console.error(err));
+const app = (0, express_1.default)();
+app.use((0, cookie_parser_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, cors_1.default)({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+}));
+app.use(express_1.default.static(path_1.default.join(__dirname, "../../frontend/dist")));
+app.use("/api/users", users_1.default);
+app.listen(4500, () => {
+    console.log(`Server is running at 4500`);
+});
